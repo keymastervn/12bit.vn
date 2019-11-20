51719ba4cbd09f23e4ba65ba45a52d327706cb63
Duoc Nguyen <nguyenvanduocit@gmail.com>
2019-11-20 13:18:40 +0700

    try tailwind

---
+++ themes/blog/postcss.config.js
@@ -0,0 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './layouts/**/*.html',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
+}
